a6a1ddcf4671ebb076cad341d66d87e29f42a2c8
叶涛 <821023977@qq.com>
2019-09-11 14:24:07 +0800

    1

---
+++ 0.6.0/FixedLengthFrameDecoder.cs
@@ -0,0 +1,36 @@
+﻿using DotNetty.Buffers;
+using DotNetty.Codecs;
+using DotNetty.Transport.Channels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer
+{
+    public class FixedLengthFrameDecoder : ByteToMessageDecoder
+    {
+        private int frameLength;
+
+        public FixedLengthFrameDecoder(int frameLength)
+        {
+            this.frameLength = frameLength;
+        }
+
+        protected override void Decode(IChannelHandlerContext context, IByteBuffer input, List<object> output)
+        {
+            object decoded = decode(context, input);
+            if (decoded != null)
+            {
+                output.Add(decoded);
+            }
+        }
+
+        protected Object decode(IChannelHandlerContext ctx, IByteBuffer input)
+        {
+            return input.ReadableBytes < this.frameLength ? null : input.ReadRetainedSlice(this.frameLength);
+        }
+    }
+}
+
